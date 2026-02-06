f51e4848a401e5db2ddc3334168b724c580437e5
Jeanpool <patrickjeam2@gmail.com>
2026-02-06 23:05:59 +0100

    just it's mental notes no more

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jezambra <jezambra@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 21:10:08 by jezambra          #+#    #+#             */
+/*   Updated: 2026/02/06 22:16:41 by jezambra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+typedef struct	s_gnl
+{
+	char	*stash;
+}				t_gnl;
+
+char	*get_next_line(int fd);
+
+
+
+#endif
