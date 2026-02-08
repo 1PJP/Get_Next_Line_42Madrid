1138719681b24d6de51cb03edc60f2b86fcffade
Jeanpool <patrickjeam2@gmail.com>
2026-02-08 21:15:47 +0100

    funciones auxiliares

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jezambra <jezambra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 21:10:08 by jezambra          #+#    #+#             */
-/*   Updated: 2026/02/08 16:11:22 by jezambra         ###   ########.fr       */
+/*   Updated: 2026/02/08 21:14:19 by jezambra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef struct	s_gnl
 
 char	*get_next_line(int fd);
 size_t	ft_strlen_gnl(char *s);
+char	*ft_strdup_gnl(char *s);
 
 
 #endif
--- Get_next_line/get_next_line_utils.c
+++ Get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jezambra <jezambra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 21:09:54 by jezambra          #+#    #+#             */
-/*   Updated: 2026/02/08 16:06:28 by jezambra         ###   ########.fr       */
+/*   Updated: 2026/02/08 21:09:33 by jezambra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,30 @@ size_t	ft_strlen_gnl(char *s)
 		i++;
 	return (i);
 }
+/*con este strlen decidimos cuanto vamos hacer crecer el stash para no hacer overflow
+y lo reutilizamos dentro del codigo para ahorra lienas, tambien se le hizo una pequena 
+modificacion ya que el stash inicia en NULL asi que para que no de error se le anadio 
+una pequena condicion */
+
+char	*ft_strdup_gnl(char *s)
+{
+	char	*str;
+	int	i;
+
+	if (!s)
+		return (NULL);
+	str = malloc(ft_strlen_gnl(s) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*con esta funcion creamos una copia de stash por que al hacer free liberamos el 
+stash pero ya tenemos una copia en memoria del stash con strdup, asi que no vamos 
+a perder nada y podremos hacer free y guardar lo que vamos a leer, free para liberar
+lo leido y strdup para hacr una copia de lo que no se a leido */
